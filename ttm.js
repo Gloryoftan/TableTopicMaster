0c549f5e82d6575e8d689c1a5cd4da3025938049
Gloryoftan <916627606@qq.com>
2019-07-24 18:32:19 +0800

    second commit

--- ttm.js
+++ ttm.js
@@ -28,7 +28,7 @@ function sceneSwitch(preSceneId, nextSceneId) {
     $('#' + nextSceneId).css('display', 'flex');
 
     // application
-    var speed = 75;
+    var speed = 15;
     var h1 = $('#' + nextSceneId + ' h1')[0];
     var aP = $('#' + nextSceneId + ' p');
     var delay = h1.innerHTML.length * speed;
@@ -44,9 +44,64 @@ function sceneSwitch(preSceneId, nextSceneId) {
     }
 
     setTimeout(function () {
-        $('#' + nextSceneId + 'BtnGroup').css('display', 'block');
+        $('#' + nextSceneId + 'BtnGroup').css('display', 'flex');
     }, delay);
 }
+
+//状态栏更新
+let iq = 0;
+let eq = 0;
+let st = 0;
+let ch = 0;
+let pressure = 0;
+let money = 0;
+let aIq = document.querySelectorAll("#TTM .status .iq .star li");
+let aEq = document.querySelectorAll("#TTM .status .eq .star li");
+let aSt = document.querySelectorAll("#TTM .status .st .star li");
+let aCh = document.querySelectorAll("#TTM .status .ch .star li");
+let oPressure = document.querySelector("#TTM .status .pressure .data");
+let oMoney = document.querySelector("#TTM .status .money .data");
+console.log(aIq);
+
+function iqAdd(iq){
+    for(let i = 0;i<iq;i++){
+        aIq[i].className = "light";
+    }
+}
+iqAdd(iq);
+
+function eqAdd(eq){
+    for(let i = 0;i<eq;i++){
+        aEq[i].className = "light";
+    }
+}
+eqAdd(eq);
+
+function stAdd(st){
+    for(let i = 0;i<st;i++){
+        aSt[i].className = "light";
+    }
+}
+stAdd(st);
+
+function chAdd(ch){
+    for(let i = 0;i<ch;i++){
+        aCh[i].className = "light";
+    }
+}
+chAdd(ch);
+
+console.log( oPressure.innerHTML);
+
+function pressureAdd(pressure){
+    oPressure.innerHTML = pressure;
+}
+pressureAdd(pressure);
+
+function moneyAdd(money){
+    oMoney.innerHTML = money;
+}
+moneyAdd(money);
 
 //scene1
 let babyName = "没有名字";
@@ -72,4 +127,22 @@ $("#scene1Btn").click(function () {
 //scene2
 $('#scene2btn').click(function () {
     sceneSwitch("scene2", "scene3");
+})
+
+//scene3
+$('#scene3BtnA').click(function () {
+    swal({
+        text: "",
+    }).then(() => {
+        iq++;
+        st++;
+        pressure++;
+        money+=100;
+        iqAdd(iq);
+        stAdd(st);
+        pressureAdd(pressure);
+        moneyAdd(money);
+        sceneSwitch("scene3", "scene5");
+    });;
+
 })
